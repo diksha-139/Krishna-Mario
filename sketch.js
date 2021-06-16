aff4e7b0ac5936d8b1ce10e20bf6aae13e0a9dab
diksha-139 <diksha.verma139@gmail.com>
2021-06-16 19:32:33 +0530

    first

--- sketch.js
+++ sketch.js
@@ -1,66 +1,44 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+var bgImg,mario,runningMario,collidedMario,enemy,pipeImg,pipe;
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+bgImg= loadImage("bg.jpg");
+runningMario=loadAnimation("m1.png","m2.png","m3.png");
+collidedMario=loadAnimation("Mcollide.png");
+pipeImg= loadImage("pipe.png");
+enemyImg1=loadImage("Mario_Enemy1.png");
+enemyImg2=loadImage("Mario_Enemy2.png")
+
+enemyImg3=loadImage("Mario_Enemy3.png")
+
 }
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
+	
 	
 
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+	
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
-	World.add(world, packageBody);
 	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-
 	Engine.run(engine);
   
 }
 
 
 function draw() {
-  rectMode(CENTER);
+ 
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+ 
   drawSprites();
  
 }
-
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	Matter.Body.setStatic(packageBody,false);
-    
-  }
-}
-
-
 
